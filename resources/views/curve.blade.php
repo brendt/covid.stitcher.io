e5237d1819df58b94fd126fbf2aaeb7d7bcd13ee
Brent Roose <brent@spatie.be>
2020-03-17 07:47:00 +0100

    wip

--- resources/views/curve.blade.php
+++ resources/views/curve.blade.php
@@ -4,7 +4,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Flatten the curve</title>
+    <title>#FlattenTheCurve</title>
+
+    <meta name="twitter:card" content="article">
+    <meta property="og:type" content="article">
+
+    <meta name="title" content="Corona progression over time">
+    <meta name="twitter:title" content="Corona progression over time">
+    <meta property="og:title" content="Corona progression over time">
+    <meta itemprop="name" content="Corona progression over time">
+
+    <meta name="description" content="Daily timeline of covid-19 cases per country">
+    <meta name="twitter:description" content="Daily timeline of covid-19 cases per country">
+    <meta property="og:description" content="Daily timeline of covid-19 cases per country">
+    <meta itemprop="description" content="Daily timeline of covid-19 cases per country">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
